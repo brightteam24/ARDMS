17bb67c3f54d6bf4ededb6d8325060ced4f8d118
Bright TechHub <157127058+brighttech24@users.noreply.github.com>
2024-06-26 12:41:57 +0100

    Update settings.py

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -89,7 +89,8 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 DATABASES = {    
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'mydatabase',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
